469c6bdf986fb4cdb1d15af50bbb4e9e24d68f75
Ethan Bailey <ebailey256@gmail.com>
2021-09-13 19:26:40 -0400

    format

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 '''
 TODO:
 queueing
@@ -17,42 +16,66 @@ import youtube_dl
 dotenv.load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 client = discord.ext.commands.Bot(command_prefix="!")
-sp = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
+sp = spotipy.Spotify(
+    client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
+
 
 @client.command()
-async def play(ctx, url: str):
+async def search(ctx, *, term):
+    await play(ctx, f'ytsearch:{term}')
+
+
+@client.command()
+async def file(ctx: discord.ext.commands.context.Context):
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice == None:
         await voiceChannel.connect()
         voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
-    if 'spotify' in url:
-        track_info = sp.track(url)
-        url = f'ytsearch:{track_info["artists"][0]["name"]} {track_info["name"]}'
+    # start player
+    voice.play(discord.FFmpegPCMAudio(ctx.message.attachments[0].url))
+
+
+@client.command()
+async def play(ctx, url):
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice == None:
+        await voiceChannel.connect()
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    # start player
+    audio_url = get_audio_stream(url)
+    voice.play(discord.FFmpegPCMAudio(audio_url))
+
+
+def get_audio_stream(term):
+    if 'spotify' in term:
+        track_info = sp.track(term)
+        term = f'ytsearch:{track_info["artists"][0]["name"]} {track_info["name"]}'
 
     ydl_opts = {
-        'format': 'bestaudio/best',
+        'format':
+        'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'skip_download': True
+        'skip_download':
+        True
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
+        info = ydl.extract_info(term, download=False)
         if 'url' not in info:
             print('searched url')
             audio_url = info['entries'][0]['url']
         else:
             print('regular url')
             audio_url = info['url']
-        print(f'playing {audio_url}')
-        voice.play(discord.FFmpegPCMAudio(audio_url))
 
-async def parse_spotify_url(url: str):
-    pass
+        return audio_url
 
 
 @client.command()
@@ -86,5 +109,6 @@ async def resume(ctx):
 async def stop(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
+
 
 client.run(token)
